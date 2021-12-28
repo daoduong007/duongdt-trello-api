a7f160aa50e97b1694a3fed1855820af4d753997
daoduong007 <daoduong.007@gmail.com>
2021-12-28 17:57:11 +0700

    init project node js

---
+++ src/server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import { mapOder } from './utilities/sorts.js';
+
+const app = express();
+
+const hostname = 'localhost';
+const port = 8017;
+
+app.get('/', (req, res) => {
+  res.end('<h1>Hello world</h1><hr/>');
+});
+
+app.listen(port, hostname, () => {
+  console.log(`hello ml, at ${hostname}:${port}`);
+});
---
+++ src/utilities/sorts.js
@@ -0,0 +1,6 @@
+const mapOder = (array, oder, key) => {
+  array.sort((a, b) => oder.indexOf(a[key]) - oder.indexOf(b[key]));
+  return array;
+};
+
+export { mapOder };

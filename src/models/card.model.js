30ed3c135bc0c3a621feb18cdef68c4b712359ff
daoduong007 <daoduong.007@gmail.com>
2021-12-31 18:45:16 +0700

    CRUD APIs

--- src/models/card.model.js
+++ src/models/card.model.js
@@ -6,7 +6,7 @@ const cardCollectionName = 'cards';
 const cardCollectionSchema = Joi.object({
   boardId: Joi.string().required(),
   columnId: Joi.string().required(),
-  title: Joi.string().required().min(3).max(20),
+  title: Joi.string().required().min(3).max(50).trim(),
   cover: Joi.string().default(null),
   createdAt: Joi.date().timestamp().default(Date.now()),
   updatedAt: Joi.date().timestamp().default(null),
@@ -25,9 +25,9 @@ const createNew = async (data) => {
     const result = await getDB()
       .collection(cardCollectionName)
       .insertOne(value);
-    return result.ops[0];
+    return result;
   } catch (error) {
-    console.log(error);
+    throw new Error(error);
   }
 };
 

ff6822145ce629c74a392bd70dc537a33bd1fc32
daoduong007 <daoduong.007@gmail.com>
2022-01-05 04:01:07 +0700

    deploy api to heroku

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import { corsOptions } from '*/config/cors';
 import { connectDB } from './config/mongodb';
-import { env } from '*/config/environment';
+// import { env } from '*/config/environment';
 import { apiV1 } from '*/routers/v1';
 
 connectDB()
@@ -24,7 +24,12 @@ const bootServer = () => {
 
   app.use('/v1', apiV1);
 
-  app.listen(env.APP_PORT, env.APP_HOST, () => {
-    console.log(`${env.APP_HOST}: ${env.APP_PORT}`);
+  // app.listen(env.APP_PORT, env.APP_HOST, () => {
+  //   console.log(`${env.APP_HOST}: ${env.APP_PORT}`);
+  // });
+
+  //support herolu deploy
+  app.listen(process.env.PORT, () => {
+    console.log(`hello , i'm running at port : ${process.env.PORT}`);
   });
 };
